8ec05d85758754816e539c83889b3441a5bee785
zhengmengli <15711372113@163.com>
2018-09-28 12:15:59 +0800

    first commit

---
+++ static/config.js
@@ -0,0 +1,5 @@
+//export const BASEURL='http://192.168.21.111:8080/aobo'
+export const BASEURL='http://192.168.20.145:8080/aobo'
+//export const BASEURL='/aobo'
+export const BASEURLMOCK=''
+export const TIMEOUT=5000
